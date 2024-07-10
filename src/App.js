8e894f769374b9f173ac4824fc96ab2318ec1103
nargess alavi <alavi_n70@yahoo.com>
2024-07-10 23:16:42 +0330

    feature
    add a object in array

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 2, description: "Charger", quantity: 1, packed: false },
+  { id: 2, description: "t-shirt", quantity: 5, packed: false },
 ];
 export default function App() {
   return (
