cbd78b02dab0c7a99c172957b9d649d9b33aa6e1
nargess alavi <alavi_n70@yahoo.com>
2024-07-11 08:11:14 +0330

    Update App.js
    add a selected in the form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import "./index.css";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "Charger", quantity: 1, packed: false },
-  { id: 2, description: "t-shirt", quantity: 5, packed: false },
-  { id: 2, description: "t-shirt", quantity: 5, packed: false },
-  { id: 2, description: "t-shirt", quantity: 5, packed: false },
-  { id: 2, description: "t-shirt", quantity: 5, packed: false },
-  { id: 2, description: "t-shirt", quantity: 5, packed: false },
-  { id: 2, description: "t-shirt", quantity: 5, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+  { id: 4, description: "T-shirt", quantity: 5, packed: false },
+  { id: 5, description: "pants", quantity: 1, packed: false },
+  { id: 6, description: "Hat", quantity: 2, packed: false },
+  { id: 7, description: "Bag", quantity: 2, packed: false },
+  { id: 8, description: "Glasses", quantity: 1, packed: false },
+  { id: 9, description: "mobile", quantity: 1, packed: false },
 ];
 export default function App() {
   return (
@@ -26,10 +26,23 @@ function Logo() {
 }
 
 function Form() {
+  function HandleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={HandleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Items..." />
+      <button>Add</button>
+    </form>
   );
 }
 
