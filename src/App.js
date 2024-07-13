31f2d340f84775eb1739288432288d2d9f343e5c
nargess alavi <alavi_n70@yahoo.com>
2024-07-13 22:31:14 +0330

    feat
    add sortby and setsortby for sort items

--- src/App.js
+++ src/App.js
@@ -82,10 +82,23 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
-    <div className="items-list">
-      <ul className="list">
-        {items.map((item) => (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -95,7 +108,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
         ))}
       </ul>
       <div className="actions">
-        <select>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
           <option value="input">Sort by input order</option>
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed Statue</option>
