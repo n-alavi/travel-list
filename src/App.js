ad3d30611182ec26ae96e6aafd3d78ac6b9dceea
nargess alavi <alavi_n70@yahoo.com>
2024-07-13 17:44:01 +0330

    feat
    add a selected with 3 option for sort items

--- src/App.js
+++ src/App.js
@@ -94,6 +94,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed Statue</option>
+        </select>
+      </div>
     </div>
   );
 }
