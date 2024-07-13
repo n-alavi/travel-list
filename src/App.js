bce41b9082041d0d4d36f480daf33d14094751ed
nargess alavi <alavi_n70@yahoo.com>
2024-07-13 17:31:27 +0330

    feat
    add alert for stats that we don't have items.length

--- src/App.js
+++ src/App.js
@@ -116,6 +116,12 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 }
 
 function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em> Start adding some items to your packing list 🧳 </em>
+      </p>
+    );
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
   const percentage = Math.round((numPacked / numItems) * 100);
